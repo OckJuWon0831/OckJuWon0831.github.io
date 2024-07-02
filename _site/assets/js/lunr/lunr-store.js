aa4e9420359dba9db39d52a09615bb9829c2c249
OckJuWon0831 <ockjuwon99@naver.com>
2024-07-03 04:33:27 +0900

    .

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": null,
-        "excerpt":"쓰레드에 대해 알아보자 Thread는 그 자체로 존재하지 못한다. 즉, process에 의존해서 존재한다. 현 Process는 최소 1개 이상의 Thread를 갖는다. Process는 관리의 단위이다. Thread는 개별화된 흐름(문맥)과 전용 스택을 갖는 실행의 단위이다. 모든 Thread는 자신이 속한 Process의 가상 메모리 공간을 공유 한다. OS -&gt; Process -&gt; Thread Operating System : 국가행정(관리를 위한)...","categories": [],
-        "tags": null,
-        "url": "/2024-07-03-thread/",
+        "title": "쓰레드(Thread)란 무엇일까?",
+        "excerpt":"쓰레드에 대해 알아보자 Thread는 그 자체로 존재하지 못한다. 즉, process에 의존해서 존재한다. 현 Process는 최소 1개 이상의 Thread를 갖는다. Process는 관리의 단위이다. Thread는 개별화된 흐름(문맥)과 전용 스택을 갖는 실행의 단위이다. 모든 Thread는 자신이 속한 Process의 가상 메모리 공간을 공유 한다. OS -&gt; Process -&gt; Thread Operating System : 국가행정(관리를 위한)...","categories": ["CS"],
+        "tags": ["Operating System","Thread"],
+        "url": "/cs/thread/",
         "teaser": null
       }]
