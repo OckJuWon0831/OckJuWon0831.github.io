572681340b5dbfc839bfd64a9a3c376b35985d6b
OckJuWon0831 <ockjuwon99@naver.com>
2024-07-05 17:51:28 +0900

    .

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "쓰레드(Thread)란 무엇일까?",
+        "title": "The concept of Thread",
         "excerpt":"쓰레드에 대해 알아보자 Thread는 그 자체로 존재하지 못한다. 즉, process에 의존해서 존재한다. 현 Process는 최소 1개 이상의 Thread를 갖는다. Process는 관리의 단위이다. Thread는 개별화된 흐름(문맥)과 전용 스택을 갖는 실행의 단위이다. 모든 Thread는 자신이 속한 Process의 가상 메모리 공간을 공유 한다. OS -&gt; Process -&gt; Thread Operating System : 국가행정(관리를 위한)...","categories": ["CS"],
         "tags": ["Operating System","Thread"],
         "url": "/cs/thread/",
+        "teaser": null
+      },{
+        "title": "On compiler",
+        "excerpt":"컴파일러 구성요소 프로그래밍은 글쓰기이다. 컴파일러를, 특히 컴파일러의 프론트엔드 단을 공부하면, 프로그래밍 언어란 무엇인가라는 주제로까지 문답이 가능해지며, 이는 전반적인 프로그래밍 언어에 대한 이해력이 향상된다. 어휘, 구문 분석 개념 문자열 형태로 존재하는 소스코드를, 의미 있는 문자 혹은 문자열 조각(토큰) 으로 변환 (예약어, 상수, 변수, 연산자 등등) 빈칸이나, 세미콜론 같은 것들은 구문문자라 하며,...","categories": ["CS"],
+        "tags": ["Operating System","Compiler"],
+        "url": "/cs/compiler/",
         "teaser": null
       }]
